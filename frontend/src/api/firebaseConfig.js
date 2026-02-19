ada34ff1ee062435007542d5131e0afddb9f0b1d
CristianBautista <emirbautista30@gmail.com>
2026-02-18 22:41:15 -0600

    Ya le cree archivos Ale haber si ya te deja

--- frontend/src/api/firebaseConfig.js
+++ frontend/src/api/firebaseConfig.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, OAuthProvider } from "firebase/auth"; // Combinados en una sola línea
+import { getAuth, OAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // O import.meta.env.VITE_FIREBASE_API_KEY
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
@@ -12,18 +12,14 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-
-// Inicializamos la App de Firebase (Solo una vez)
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
-// Configuración para Microsoft (Azure AD)
+// Configuración para el login institucional de Microsoft
 export const microsoftProvider = new OAuthProvider('microsoft.com');
-
-// Forzamos a que pida la cuenta de la escuela específicamente
 microsoftProvider.setCustomParameters({
   prompt: 'select_account',
-  tenant: 'common' // O el ID de inquilino específico de tu escuela si lo tienes
+  tenant: 'common'
 });
